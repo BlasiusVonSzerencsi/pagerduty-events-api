dd993450cb8509bff14cb1e6fa20dee76f925e0a
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-06-23 00:21:03 +0200

    Trigger PagerDuty event with description.

--- pagerduty_events_api/pagerduty_service.py
+++ pagerduty_events_api/pagerduty_service.py
@@ -1,6 +1,18 @@
+import json
+import requests
+
+
 class PagerdutyService:
     def __init__(self, key):
         self.__key = key
 
     def get_key(self):
         return self.__key
+
+    def trigger(self, description):
+        payload = {'service_key': self.__key,
+                   'event_type': 'trigger',
+                   'description': description}
+
+        requests.post('https://events.pagerduty.com/generic/2010-04-15/create_event.json',
+                      json.dumps(payload))
--- pagerduty_events_api/tests/test_pagerduty_service.py
+++ pagerduty_events_api/tests/test_pagerduty_service.py
@@ -1,4 +1,8 @@
+import json
+import requests
+
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 from pagerduty_events_api import PagerdutyService
 
@@ -8,3 +12,13 @@ class TestPagerduty(TestCase):
         subject = PagerdutyService('my_service_key')
 
         self.assertEqual('my_service_key', subject.get_key())
+
+    def test_trigger_should_make_pagerduty_api_call(self):
+        requests.post = MagicMock()
+
+        PagerdutyService('my_service_key').trigger('some_description')
+
+        requests.post.assert_called_once_with('https://events.pagerduty.com/generic/2010-04-15/create_event.json',
+                                              json.dumps({'service_key': 'my_service_key',
+                                                          'event_type': 'trigger',
+                                                          'description': 'some_description'}))
