e0aa51e1677ff2aaf66236f50800c1c4a7c28a57
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-06-23 15:31:30 +0200

    Acknowledge PagerDuty incidents.

--- pagerduty_events_api/pagerduty_incident.py
+++ pagerduty_events_api/pagerduty_incident.py
@@ -1,3 +1,7 @@
+import json
+import requests
+
+
 class PagerdutyIncident:
     def __init__(self, service_key, incident_key):
         self.__service_key = service_key
@@ -8,3 +12,11 @@ class PagerdutyIncident:
 
     def get_incident_key(self):
         return self.__incident_key
+
+    def acknowledge(self):
+        payload = {'service_key': self.__service_key,
+                   'event_type': 'acknowledge',
+                   'incident_key': self.__incident_key}
+
+        requests.post('https://events.pagerduty.com/generic/2010-04-15/create_event.json',
+                      json.dumps(payload))
--- pagerduty_events_api/tests/test_pagerduty_incident.py
+++ pagerduty_events_api/tests/test_pagerduty_incident.py
@@ -1,4 +1,8 @@
+import json
+import requests
+
 from unittest import TestCase
+from unittest.mock import MagicMock
 
 from pagerduty_events_api import PagerdutyIncident
 
@@ -13,3 +17,13 @@ class TestPagerdutyIncident(TestCase):
         subject = PagerdutyIncident('my_service_key', 'my_incident_key')
 
         self.assertEqual('my_incident_key', subject.get_incident_key())
+
+    def test_acknowledge_should_make_pagerduty_api_call(self):
+        requests.post = MagicMock()
+
+        PagerdutyIncident('my_service_key', 'my_incident_key').acknowledge()
+
+        requests.post.assert_called_once_with('https://events.pagerduty.com/generic/2010-04-15/create_event.json',
+                                              json.dumps({'service_key': 'my_service_key',
+                                                          'event_type': 'acknowledge',
+                                                          'incident_key': 'my_incident_key'}))
