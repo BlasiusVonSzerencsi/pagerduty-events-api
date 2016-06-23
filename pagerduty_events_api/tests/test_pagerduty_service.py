d3ffb2d21de3cacb489a66bdaa3cea0cd6772a9d
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-06-23 17:55:21 +0200

    Use / mock PagerDuty rest client in tests.

--- pagerduty_events_api/tests/test_pagerduty_service.py
+++ pagerduty_events_api/tests/test_pagerduty_service.py
@@ -1,9 +1,5 @@
-import json
-import requests
-
 from unittest import TestCase
-from unittest.mock import MagicMock
-from unittest.mock import Mock
+from unittest.mock import patch
 
 from pagerduty_events_api import PagerdutyService
 from pagerduty_events_api import PagerdutyIncident
@@ -17,28 +13,27 @@ class TestPagerdutyService(TestCase):
     def test_get_service_key_should_return_the_service_key(self):
         self.assertEqual('my_service_key', self.__subject.get_service_key())
 
-    def test_trigger_should_make_pagerduty_api_call(self):
-        response = Mock(text='{"incident_key": "my_incident_key"}')
-        requests.post = MagicMock(return_value=response)
+    @patch('pagerduty_events_api.pagerduty_rest_client.PagerdutyRestClient.post')
+    def test_trigger_should_make_pagerduty_api_call(self, post):
+        post.return_value = {'incident_key': 'my_incident_key'}
 
         self.__subject.trigger('some_description')
 
-        requests.post.assert_called_once_with('https://events.pagerduty.com/generic/2010-04-15/create_event.json',
-                                              json.dumps({'service_key': 'my_service_key',
-                                                          'event_type': 'trigger',
-                                                          'description': 'some_description'}))
+        post.assert_called_once_with({'service_key': 'my_service_key',
+                                      'event_type': 'trigger',
+                                      'description': 'some_description'})
 
-    def test_trigger_should_return_a_pagerduty_incident(self):
-        response = Mock(text='{"incident_key": "my_incident_key"}')
-        requests.post = MagicMock(return_value=response)
+    @patch('pagerduty_events_api.pagerduty_rest_client.PagerdutyRestClient.post')
+    def test_trigger_should_return_a_pagerduty_incident(self, post):
+        post.return_value = {'incident_key': 'my_incident_key'}
 
         incident = self.__subject.trigger('some_description')
 
         self.assertIsInstance(incident, PagerdutyIncident)
 
-    def test_trigger_should_parse_the_api_response(self):
-        response = Mock(text='{"incident_key": "my_incident_key"}')
-        requests.post = MagicMock(return_value=response)
+    @patch('pagerduty_events_api.pagerduty_rest_client.PagerdutyRestClient.post')
+    def test_trigger_should_parse_the_api_response(self, post):
+        post.return_value = {'incident_key': 'my_incident_key'}
 
         incident = self.__subject.trigger('some_description')
 
